431393029cd65ef4789693b67eb2411e9001244a
johnatanf <johnatan.f100@gmail.com>
2024-08-28 10:26:38 +0700

    implement logout functionality

--- src/components/Profile/index.jsx
+++ src/components/Profile/index.jsx
@@ -1,16 +1,24 @@
-import useUserStore from '../../store/useUserStore'
-import './Profile.module.css'
+import useUserStore from "../../store/useUserStore";
+import loginService from "../../services/loginService";
+import "./Profile.module.css";
 
 function Profile() {
-  const user = useUserStore((state) => state.user)
+  const user = useUserStore((state) => state.user);
+
+  const handleLogout = async () => {
+    try {
+      await loginService.logoutUser();
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   return (
     <>
-      <div>
-        { user ? <p>{user.email} currently logged in</p> : null }
-      </div>
+      <div>{user ? <p>{user.email} currently logged in</p> : null}</div>
+      <button onClick={handleLogout}>Logout</button>
     </>
-  )
+  );
 }
 
-export default Profile
+export default Profile;
--- src/services/loginService.js
+++ src/services/loginService.js
@@ -1,25 +1,51 @@
 import useUserStore from "../store/useUserStore";
-import axios from 'axios';
+import axios from "axios";
 
-const apiUrl = import.meta.env.VITE_REACT_APP_API_URL
+const apiUrl = import.meta.env.VITE_REACT_APP_API_URL;
 
 const loginUser = async (email, password) => {
   const { setUser, setIsAuthenticated } = useUserStore.getState();
 
   try {
-    const response = await axios.post(`${apiUrl}/login`, { email, password_hash: password }, {withCredentials: true})
-    
+    const response = await axios.post(
+      `${apiUrl}/login`,
+      { email, password },
+      { withCredentials: true }
+    );
+
     setUser({ email });
-    setIsAuthenticated(true)
+    setIsAuthenticated(true);
 
     return {
-      message: "successful",
+      message: "successful login",
     };
   } catch (error) {
     console.error("Error logging in:", error);
-    setIsAuthenticated(false)
+    setIsAuthenticated(false);
     throw error;
   }
 };
 
-export default { loginUser };
+const logoutUser = async () => {
+  const { setUser, setIsAuthenticated } = useUserStore.getState();
+
+  try {
+    const response = await axios.post(`${apiUrl}/auth/logout`, {
+      withCredentials: true,
+    });
+
+    setUser({});
+    setIsAuthenticated(false);
+
+    return {
+      message: "successful logout",
+    };
+  } catch (error) {
+    console.error("Error logging out:", error);
+    setUser({});
+    setIsAuthenticated(false);
+    throw error;
+  }
+};
+
+export default { loginUser, logoutUser };
